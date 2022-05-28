fbe5e836242cc7fa8b24a16560966ad2192bbc43
bettyfileti <beth@politetype.com>
2022-05-28 19:48:15 -0400

    First commit

---
+++ js/headline.js
@@ -0,0 +1,157 @@
+const Y_AXIS = 1;
+const X_AXIS = 2;
+
+class Headline {
+    constructor(eyebrow, text, style, numOfLines, subtext) {
+        this.eyebrowText = eyebrow,
+            this.headlineText = text,
+            this.style = style,
+            this.active = false,
+            this.numOfLines = numOfLines,
+            this.subtext = subtext
+    }
+
+    draw() {
+        let eyebrowSize = 24;
+        let headlineSize = 28;
+        let headlineLeading = 28;
+
+        let margin = 10;
+        let lowerThird = height / 5;
+        let eyebrowBG = {
+            x: margin,
+            y: height - lowerThird - (margin * 2.5),
+            width: (eyebrowSize / 1.5) * this.eyebrowText.length,
+            height: 32
+        }
+
+        let headlineBG = {
+            x: margin,
+            y: eyebrowBG.y + eyebrowBG.height,
+            width: width - (margin * 2),
+            height: 70
+        }
+        noStroke()
+
+        if (this.style === "msnbc") {
+            //grey background
+            push();
+            fill("lightgrey");
+            stroke("lightgrey");
+            strokeWeight(2);
+            rect(0, height - lowerThird, width, lowerThird);
+
+            //eyebrow
+
+            fill("white");
+            rect(eyebrowBG.x, eyebrowBG.y, eyebrowBG.width, eyebrowBG.height);
+            fill("#831811")
+            textSize(eyebrowSize);
+            textFont(futura);
+            text(this.eyebrowText, eyebrowBG.x + margin, eyebrowBG.y + eyebrowSize - 2);
+
+            //headline
+            rect(headlineBG.x, headlineBG.y, headlineBG.width, headlineBG.height);
+
+            fill("white");
+            textFont(futuraCondensed);
+            textSize(headlineSize);
+            textAlign(CENTER);
+            textLeading(headlineLeading);
+            if (this.numOfLines === 1) {
+                text(this.headlineText, headlineBG.x + margin, headlineBG.y + (margin * 2), headlineBG.width - (margin * 2));
+            } else {
+                text(this.headlineText, headlineBG.x + margin, headlineBG.y + margin, headlineBG.width - (margin * 2));
+            }
+            pop();
+        } else if (this.style === "cnn") {
+            push();
+
+            //eyebrow
+            fill("#b02418");
+            noStroke();
+            eyebrowBG.x = 0;
+            eyebrowBG.y = eyebrowBG.y + (margin * 2);
+            eyebrowBG.width = eyebrowBG.width + 20;
+            eyebrowBG.height = height - eyebrowBG.y;
+            rect(eyebrowBG.x, eyebrowBG.y, eyebrowBG.width, eyebrowBG.height);
+
+            fill("white");
+            textFont("helvetica");
+            textStyle(BOLD);
+            textSize(eyebrowSize);
+            text(this.eyebrowText, eyebrowBG.x + margin, eyebrowBG.y + eyebrowSize);
+
+
+            //headline
+            headlineBG.y = eyebrowBG.y + 30;
+            headlineBG.x = headlineBG.x - (margin / 2);
+            headlineBG.height = height - headlineBG.y - margin;
+            rect(headlineBG.x, headlineBG.y, headlineBG.width, headlineBG.height);
+
+            fill("black");
+            textFont("nimbus-sans-condensed");
+            textSize(headlineSize);
+            if (this.numOfLines === 1) {
+                scale(0.675, 1.2);
+                text(this.headlineText, headlineBG.x + margin, headlineBG.y - (margin * 3.25));
+            } else {
+                textLeading(26);
+                scale(0.675, 1.0);
+                textSize(headlineSize - 2);
+                text(this.headlineText, headlineBG.x + margin, headlineBG.y + (margin / 1.5), headlineBG.width * 1.5, headlineBG.height * 2);
+            }
+            pop();
+
+        } else if (this.style = "abc") {
+            push();
+
+            //eyebrow
+            fill("white");
+            eyebrowBG.width = width - (margin * 2);
+            rect(eyebrowBG.x, eyebrowBG.y, eyebrowBG.width, eyebrowBG.height, 2);
+            fill("#242e75");
+            textSize(eyebrowSize);
+            textFont("futura-pt");
+            text(this.eyebrowText, eyebrowBG.x + margin, eyebrowBG.y + eyebrowSize);
+
+            //header
+            //setGradient(50, 190, 540, 80, "#242e75", "#ffffff", X_AXIS);
+            fill("#15329b");
+            textFont("futura-pt-bold");
+            textSize(21);
+            rect(headlineBG.x, headlineBG.y, headlineBG.width, headlineBG.height, 2);
+            fill("white");
+            text(this.headlineText, headlineBG.x + margin, headlineBG.y + (margin * 1.5), headlineBG.width - (margin * 2));
+            textFont("futura-pt");
+            textSize(15);
+            text(this.subtext, headlineBG.x + margin, headlineBG.y + margin + (eyebrowSize * 1.25), headlineBG.width - (margin * 2));
+            pop();
+        }
+
+    }
+}
+
+//--------------------------------------------------------------
+function setGradient(x, y, w, h, c1, c2, axis) {
+    noFill();
+    c1 = color(c1);
+    c2 = color(c2);
+    if (axis === Y_AXIS) {
+        // Top to bottom gradient
+        for (let i = y; i <= y + h; i++) {
+            let inter = map(i, y, y + h, 0, 1);
+            let c = lerpColor(c1, c2, inter);
+            stroke(c);
+            line(x, i, x + w, i);
+        }
+    } else if (axis === X_AXIS) {
+        // Left to right gradient
+        for (let i = x; i <= x + w; i++) {
+            let inter = map(i, x, x + w, 0, 1);
+            let c = lerpColor(c1, c2, inter);
+            stroke(c);
+            line(i, y, i, y + h);
+        }
+    }
+}
---
+++ js/sketch.js
@@ -0,0 +1,222 @@
+// This example code is created based on:
+// https://github.com/tensorflow/tfjs-models/tree/master/pose-detection/src/blazepose_tfjs#usage
+
+let cam;
+let pose;
+let detector;
+let recentLeftXs = [];
+let recentRightXs = [];
+let recentLeftYs = [];
+let recentRightYs = [];
+let numXs = 5; //this is the number of frames to average
+let weArePraying = false;
+let headlines = [];
+let futura = "futura-pt-bold";
+let futuraCondensed = "futura-pt-condensed";
+let helvetica = "helvetica";
+let activeHeadline;
+
+function setup() {
+  let cnv = createCanvas(640, 480);
+  cnv.parent('canvas-container');
+
+  background("#1B4D92");
+
+  setupHeadlines();
+
+  cam = createCapture(VIDEO, camReady);
+  cam.size(640, 480);
+  cam.hide();
+
+  textFont(helvetica);
+  activateRandomHeadline();
+}
+
+function draw() {
+  image(cam, 0, 0);
+
+  if (pose != undefined) {
+    let pinkyDistance;
+    let leftPinky = pose.keypoints[17];
+    let rightPinky = pose.keypoints[18];
+
+    displayData(leftPinky);
+    displayData(rightPinky);
+    for (let p of pose.keypoints) {
+
+      if (leftPinky.score > .10 && rightPinky.score > .10) {
+        if (leftPinky.y >= rightPinky.y + 150 || leftPinky.y <= rightPinky.y - 150){
+          // console.log("pinky fingers are too far apart");
+          let myAveragedLeftPinkyYPos = averageXPos(leftPinky.y, recentLeftYs)
+          let myAveragedRightPinkyYPos = averageXPos(leftPinky.y, recentRightYs)
+          pinkyDistance = 1000;// + leftPinky.score;
+        } else {
+          let myAveragedLeftPinkyXPos = averageXPos(leftPinky.x, recentLeftXs);
+          let myAveragedRightPinkyXPos = averageXPos(rightPinky.x, recentRightXs);
+          pinkyDistance = dist(myAveragedLeftPinkyXPos, leftPinky.y, myAveragedRightPinkyXPos, leftPinky.y)
+        }
+      } else {
+        pinkyDistance = 2000; //+ leftPinky.score;
+        // console.log("Not confident on pinky positions. Left: ", leftPinky.score, "Right: ", rightPinky.score);
+      }
+
+      if (pinkyDistance < 60) {
+        if (weArePraying === false) {
+          activeHeadline = random(headlines);
+        }
+        for (headline of headlines) {
+          if (activeHeadline.headlineText === headline.headlineText) {
+            headline.active = true;
+          } else {
+            headline.active = false;
+          }
+        }
+        weArePraying = true;
+      } else {
+        weArePraying = false;
+      }
+    }
+
+
+    for (let headline of headlines) {
+      if (headline.active === true) {
+        headline.draw();
+      }
+    }
+
+    if (weArePraying && frameCount % 60 === 0) { //higher number here makes speed slower
+      activateRandomHeadline();
+    }
+
+    //Display info
+    if (pinkyDistance != undefined) {
+      push();
+      textSize(12);
+      fill("white");
+      rect(0, 0, 100, 20);
+      fill("black");
+      text("dist:" + pinkyDistance.toFixed(2), 10, 15);
+
+      fill("white");
+      rect(0, 20, 100, 20);
+      fill("black");
+      if (weArePraying) {
+        text("PRAYING", 10, 35);
+      } else {
+        text("NOT PRAYING", 10, 35);
+      }
+    }
+
+  }
+}
+
+//--------------------------------------------------------------
+// Helper Functions
+
+function camReady() {
+  console.log("Webcam Ready!");
+  loadPoseDetectionModel();
+}
+
+function displayData(posePosition) {
+  push();
+  translate(posePosition.x, posePosition.y);
+  noStroke();
+  fill(0, 255, 0);
+  circle(0, 0, 5);
+  text(posePosition.name, 10, 10);
+  text("score:" + posePosition.score.toFixed(2), 10, 25);
+  text("x,y:" + posePosition.x.toFixed(2) + " , " + posePosition.y.toFixed(2), 10, 40);
+  pop();
+}
+
+function averageXPos(x, myArray) {
+  //This is from https://javascript.plainenglish.io/simple-smoothing-for-posenet-keypoints-cd1bc57f5872
+  if (myArray.length < 1) {
+    for (let i = 0; i < numXs; i++) {
+      myArray.push(x);
+    }
+  } else {
+    myArray.shift(); // removes first item from array
+    myArray.push(x); // adds new x to end of array
+  }
+  // add up all the values in the array
+  let sum = 0;
+  for (let i = 0; i < myArray.length; i++) {
+    sum += myArray[i];
+  }
+  // return the average x value
+  return sum / myArray.length;
+}
+
+
+async function loadPoseDetectionModel() {
+  const model = poseDetection.SupportedModels.BlazePose;
+  const detectorConfig = {
+    runtime: "tfjs",
+    enableSmoothing: true,
+    modelType: "full",  // (i.e., 'lite', 'full', 'heavy')
+  };
+  detector = await poseDetection.createDetector(model, detectorConfig);
+  console.log("Model Loaded!");
+
+  getPoses();
+}
+
+async function getPoses() {
+  const estimationConfig = { flipHorizontal: true };
+  const timestamp = performance.now();
+  const poses = await detector.estimatePoses(
+    cam.elt,
+    estimationConfig,
+    timestamp
+  );
+
+  pose = poses[0];
+
+  getPoses();
+}
+
+function setupHeadlines() {
+  headlines.push(new Headline("> BREAKING NEWS", "ALLEGED BUFFALO SHOOTER POSTED ONLINE ABOUT 'GREAT REPLACEMENT' CONSPIRACY THEORY DAYS BEFORE ATTACK", "msnbc", 2))
+  headlines.push(new Headline("> BREAKING NEWS", "15 DEAD IN TX, U.S. MOURNS ANOTHER\nMASS SHOOTING THIS MONTH", "msnbc", 2))
+  headlines.push(new Headline("> BREAKING NEWS", "TX GOV: 14 KIDS AND 1 TEACHER KILLED IN TX\nELEMENTARY SCHOOL SHOOTING", "msnbc", 2))
+  headlines.push(new Headline("> BREAKING NEWS", "TX GOV: 15 VICTIMS IN MASS SHOOTING TX\nAT TEXAS SCHOOL, SHOOTER DEAD", "msnbc", 2))
+  headlines.push(new Headline("> BREAKING NEWS", "BIDEN TO ADDRESS NATION ON TX SHOOTING AT 8:15 PM ET", "msnbc", 1))
+  headlines.push(new Headline("> BREAKING NEWS", "AT LEAST 10 KILLED, 3 INJURED IN BUFFALO MASS SHOOTING", "msnbc", 1))
+
+  headlines.push(new Headline("BREAKING NEWS", "TX GOV: 14 CHILDREN, 1 TEACHER DEAD IN ELEMENTARY SCHOOL SHOOTING", "cnn", 1));
+  headlines.push(new Headline("BREAKING NEWS", "TEXAS GOV: 15 DEAD IN ELEMENTARY SCHOOL SHOOTING IN UVALDE,\nINCLUDING 14 STUDENTS AND 1 TEACHER; SUSPECT ALSO DEAD", "cnn", 2));
+  headlines.push(new Headline("BREAKING NEWS", "TEXAS GOV: 14 CHILDREN, 1 TEACHER DEAD IN ELEMENTARY SCHOOL SHOOTING; GUNMAN IS 18-YEAR-OLD MALE; HAD A HANDGUN, POSSIBLY A RIFLE", "cnn", 2));
+  headlines.push(new Headline("BREAKING NEWS", "TEXAS GOV: 15 DEAD IN ELEMENTARY SCHOOL SHOOTING IN UVALDE,\nA CITY OF ABOUT 16,000 PEOPLE ABOUT 80 MILES WEST OF SAN ANTONIO", "cnn", 2));
+  headlines.push(new Headline("BREAKING NEWS", "SHERIFF: BUFFALO SHOOTING WAS A RACIALLY-MOTIVATED HATE CRIME", "cnn", 1));
+  headlines.push(new Headline("BREAKING NEWS", "POLICE: 10 PEOPLE KILLED IN BUFFALO SUPERMARKET SHOOTING", "cnn", 1));
+  headlines.push(new Headline("BREAKING NEWS", "GOV. HOCHUL: THIS WAS A MILITARY STYLE EXECUTION OF PEOPLE WHO SIMPLY WANTED TO BUY GROCERIES", "cnn", 2));
+  headlines.push(new Headline("BREAKING NEWS", "POLICE: SUSPECT WORE TACTICAL GEAR, LIVESTREAMED SHOOTING", "cnn", 1));
+  headlines.push(new Headline("BREAKING NEWS", "BUFFALO POLICE: SHOOTING SUSPECT IS A WHITE, 18-YEAR-OLD MALE", "cnn", 1));
+  headlines.push(new Headline("BREAKING NEWS", "FBI: BUFFALO MASS SHOOTING BEING INVESTIGATED AS HATE CRIME", "cnn", 1));
+  headlines.push(new Headline("BREAKING NEWS", "SOURCES: PURPORTED MANIFESTO LINKED TO SHOOTING UNDER REVIEW", "cnn", 1));
+  headlines.push(new Headline("BREAKING NEWS", "BUFFALO MAYOR: SHOOTER CAME FROM HOURS AWAY TO COMMIT CRIME", "cnn", 1));
+  headlines.push(new Headline("BREAKING NEWS", "BUFFALO MASS SHOOTING SUSPECT ARRAIGNED ON MURDER CHARGES", "cnn", 1));
+  headlines.push(new Headline("BREAKING NEWS", "POLICE: 10 PEOPLE KILLED IN BUFFALO SUPERMARKET SHOOTING", "cnn", 1));
+
+  headlines.push(new Headline("NEW THIS MORNING", "PRESIDENT BIDEN HEADS TO BUFFALO", "abc", 2, "WILL MEET WITH VICTIMS' FAMILIES IN WAKE OF DEADLY SUPERMARKET SHOOTING"));
+  headlines.push(new Headline("DEVELOPING STORY", "DEADLY SUPERMARKET SHOOTING INVESTIGATION", "abc", 2, "NEW DETAILS ON SUSPECT AS PRESIDENT BIDEN HEADS TO BUFFALO"));
+  headlines.push(new Headline("DEVELOPING STORY", "NEW DETAILS IN SUPERMARKET SHOOTING INVESTIGATION", "abc", 2, "POLICE: SUSPECT PLANNED DEADLY BUFFALO RAMPAGE FOR MONTHS"));
+  headlines.push(new Headline("DEVELOPING STORY", "DEADLY SUPERMARKET SHOOTING INVESTIGATION", "abc", 2, "SURVIVORS OF DEADLY RAMPAGE SHARE THEIR HARROWING STORY"));
+  headlines.push(new Headline("DEVELOPING STORY", "REMEMBERING THE VICTIMS", "abc", 2, "10 KILLED IN DEADLY BUFFALO SUPERMARKET RAMPAGE"));
+  headlines.push(new Headline("DEVELOPING STORY", "DEADLY SUPERMARKET SHOOTING INVESTIGATION", "abc", 2, "NEW QUESTIONS ABOUT MISSED WARNING SIGNS & SUSPECT'S PATH TO RADICALIZATION"));
+  headlines.push(new Headline("DEVELOPING STORY", "DEADLY SUPERMARKET SHOOTING INVESTIGATION", "abc", 2, "NEW QUESTIONS ABOUT MISSED WARNING SIGNS & SUSPECT'S PATH TO RADICALIZATION"));
+
+}
+
+function activateRandomHeadline() {
+  activeHeadline = random(headlines);
+  for (headline of headlines) {
+    if (activeHeadline.headlineText === headline.headlineText) {
+      headline.active = true;
+    } else {
+      headline.active = false;
+    }
+  }
+}
